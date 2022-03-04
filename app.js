b8759bda47c72f0b3144cdf3ac8ad383df1f42d4
ritaadhikari <ritaadhikari936@gmail.com>
2022-03-04 22:34:43 +0545

    my first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let a=1;
+let b=2;
+let c=3;
+console.log(a);
+if(a>b&&a>c){
+    console.log("a is gretest")
+}
+else if(b>c&&b>a){
+
+    console.log("b is greatest")
+}
+else{
+    console.log("c is greatest")
+}
